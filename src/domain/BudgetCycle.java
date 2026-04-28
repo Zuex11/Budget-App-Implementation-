e71bee7ee27bd1b715b02669a32e86709e73cacf
Zuex11 <moodyehab321@gmail.com>
2026-04-28 17:46:10 +0300

    Add domain implementation

--- src/domain/BudgetCycle.java
+++ src/domain/BudgetCycle.java
@@ -1,4 +1,67 @@
 package domain;
-public class BudgetCycle {
 
+import java.time.LocalDate;
+
+public class BudgetCycle {
+    private int id;
+    private double totalAllowance;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private CycleStatus status;
+    private double totalSpent;
+
+    public BudgetCycle(double totalAllowance, LocalDate startDate, LocalDate endDate) {
+        this.totalAllowance = totalAllowance;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = CycleStatus.UNINITIALIZED;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStatus(CycleStatus status) {
+        this.status = status;
+    }
+
+    public void setTotalSpent(double totalSpent) {
+        this.totalSpent = totalSpent;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getTotalAllowance() {
+        return totalAllowance;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public CycleStatus getStatus() {
+        return status;
+    }
+
+    public int getRemainingDays() {
+        return (int) java.time.temporal.ChronoUnit.DAYS.between(LocalDate.now(), endDate);
+    }
+
+    public double getRemainingBalance() {
+        return totalAllowance - totalSpent;
+    }
+
+    public double getSpentPercentage() {
+        return (totalSpent / totalAllowance) * 100;
+    }
+
+    public boolean isActive() {
+        return status == CycleStatus.ACTIVE;
+    }
 }
--- src/domain/Category.java
+++ src/domain/Category.java
@@ -1,5 +1,27 @@
 package domain;
 
-public class Category{
+public class Category {
+    private int id;
+    private String name;
+    private int iconResId;
+    public Category(int id, String name, int iconResId) {
+        this.id = id;
+        this.name = name;
+        this.iconResId = iconResId;
+    }
+    public int getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getIconResId() {
+        return iconResId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
